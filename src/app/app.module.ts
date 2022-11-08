be1f5677042a24ce5941d3f9b2ad9340b1a7d52b
mina <minabahaghighat@gmaiol.com>
2022-11-08 14:09:59 +0330

    add persian date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  DateAdapter,
+  MAT_DATE_LOCALE,
+  MatNativeDateModule,
+  MAT_DATE_FORMATS,
+} from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+
+import {
+  MaterialPersianDateAdapter,
+  PERSIAN_DATE_FORMATS,
+} from "../shared/datepicker/material.persian-date.adapter";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule
   ],
-  providers: [],
+  exports:[
+    MatDatepickerModule
+  ],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MaterialPersianDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: PERSIAN_DATE_FORMATS },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
